f3afc972fbf16ff6f44734447a27ad9c9e9b50b0
Milan Vasic <vaske494@gmail.com>
2017-11-06 18:24:06 +0100

    Add Signup Component

--- UserFront/src/main/java/com/userfront/controller/HomeController.java
+++ UserFront/src/main/java/com/userfront/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.userfront.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,8 +27,34 @@ public class HomeController {
 	public String signup(Model model) {
 		User user = new User();
 		
-		model.addAttribute("user", user);
+		model.addAttribute("user", user); //Ovaj user objekat koristimo u signup.html
 		
-		return "signup";
+		return "signup"; //signup.html
 	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public void signupPost(@ModelAttribute("user") User user, Model model) {
+		/*
+		if(userService.checkUserExist(user.getUsername(), user.getEmail())) {
+			
+			if(userService.checkEmailExists(user.getEmail())) {
+				model.addAttribute("emailExists", true);
+			}
+			
+			if(user.Service.checkUsernameExists(user.getUsername())) {
+				model.addAttribute("usernameExists", true);
+			}
+			return "signup";
+			
+		}
+		else {
+			
+			Set<UserRole> userRoles = new HashSet<>();
+			userRoles.add(new userRole(user, roleDao.findByName("USER")));
+			userService.createUser(user, userRoles);
+		}
+		*/
+		
+	}
+	
 }
