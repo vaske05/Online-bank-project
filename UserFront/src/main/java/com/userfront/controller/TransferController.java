9909ca92c66e283d043625d8bd73b36a43d6b559
Milan Vasic <vaske494@gmail.com>
2017-11-17 15:50:22 +0100

    Add recipitent functionality.

--- UserFront/src/main/java/com/userfront/controller/TransferController.java
+++ UserFront/src/main/java/com/userfront/controller/TransferController.java
@@ -1,6 +1,7 @@
 package com.userfront.controller;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.Recipient;
 import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.User;
 import com.userfront.service.TransactionService;
@@ -48,6 +50,18 @@ public class TransferController {
 		transactionService.betweenAccountsTransfer(transferFrom, transferTo, amount, primaryAccount, savingsAccount);
 		
 		return "redirect:/userFront";
+	}
+	
+	@RequestMapping(value = "/recipient", method = RequestMethod.GET)
+	public String recipient(Model model, Principal principal) {
+		List<Recipient> recipientList = transactionService.findRecipientList(principal);
+		
+		Recipient recipient = new Recipient();
+		
+		model.addAttribute("recipientList", recipientList);
+		model.addAttribute("recipient", recipient);
+		
+		return "recipient";
 	}
 
 }
